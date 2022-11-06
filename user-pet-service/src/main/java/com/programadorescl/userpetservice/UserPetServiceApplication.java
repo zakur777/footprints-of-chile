87e82f4c65af4f6fe5f289c4f5f4539baf9487f9
zakur <walter.barra@outlook.com>
2022-11-06 20:50:44 -0300

    first commit

---
+++ user-pet-service/src/main/java/com/programadorescl/userpetservice/UserPetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programadorescl.userpetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPetServiceApplication.class, args);
+	}
+
+}
---
+++ user-pet-service/src/test/java/com/programadorescl/userpetservice/UserPetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programadorescl.userpetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
