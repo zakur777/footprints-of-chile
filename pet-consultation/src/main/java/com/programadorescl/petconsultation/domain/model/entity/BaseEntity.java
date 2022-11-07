6b379b752f2e2ee7b26b70ded795baf4a8ec448b
zakur <walter.barra@outlook.com>
2022-11-06 22:19:48 -0300

    entities

---
+++ pet-consultation/src/main/java/com/programadorescl/petconsultation/domain/model/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.programadorescl.petconsultation.domain.model.entity;
+
+public abstract class BaseEntity<T> extends Entity<T> {
+
+    private boolean isModified;
+
+    public BaseEntity(T id, String name) {
+        super.id = id;
+        super.name = name;
+        isModified = false;
+    }
+
+    public boolean isIsModified() {
+        return isModified;
+    }
+
+}
